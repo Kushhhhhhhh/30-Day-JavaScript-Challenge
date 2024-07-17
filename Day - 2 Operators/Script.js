b25603bd80157029b0868c2627a8c050d828f1bb
kush sharma <kush.73888@gmail.com>
2024-07-17 15:03:58 +0530

    Day - 2 Completed

---
+++ Day - 2 Operators/Script.js
@@ -0,0 +1,90 @@
+// Activity 1 - Arithmetic Operators
+
+// Task 1
+let a = 10;
+let b = 5;
+let result = a + b;
+console.log(result);
+
+// Task 2
+let c = 10;
+let d = 5;
+let result2 = c - d;
+console.log(result2);
+
+// Task 3
+let e = 10;
+let f = 5;
+let result3 = e * f;
+console.log(result3);
+
+// Task 4 
+let g = 10;
+let h = 5;
+let result4 = g / h;
+console.log(result4);
+
+// Task 5
+let i = 10;
+let j = 5;
+let result5 = i % j;
+console.log(result5);
+
+// Activity 2 - Assignment Operators
+
+// Task 6
+let k = 10;
+k += 5;
+console.log(k);
+
+// Task 7
+let l = 10;
+l -= 5;
+console.log(l);
+
+// Activity 3 - Comparison Operators
+
+// Task 8
+let m = 10;
+let n = 5;
+let result6 = m > n;
+console.log(result6); // shows result in boolean
+
+// Task 9
+let o = 10;
+let p = 5;
+let result7 = o <= p;
+console.log(result7); 
+
+// Task 10
+let q = 10;
+let r = 10;
+let result8 = q == r || q === r;
+console.log(result8);
+
+// Activity 4 - Logical Operators
+
+// Task 11
+let s = 10;
+let t = 5;
+let result9 = s > t && s < t;
+console.log(result9);
+
+// Task 12
+let u = 10;
+let v = 5;
+let result10 = u > v || u < v;
+console.log(result10);
+
+// Task 13
+let w = 10;
+let x = 5;
+let result11 = !(w < x);
+console.log(result11);
+
+// Activity 5 - Ternary Operator
+
+// Task 14
+let y = 50;
+let result12 = y > 0 ? "positive" : "negative";
+console.log(result12);
